6dd2ef21611c555b29dc47ae1ad1be380d5aea37
Gianmatco <gianmarco.culqui.ikaros@gmail.com>
2022-03-31 17:12:32 +0200

    uplo

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+
+document.getElementById('place');
+place.setAttribute('class' ,'.text');
+place = document.write(title[0]);
+
+
+
+
+
+
+
+
+
+let currentIndex = 0;
+const listaImmagini = document.getElementsByClassName("item");
+const listaSlide = document.getElementsByClassName("mySlides1");
+
+const next = document.querySelector(".next");
+next.addEventListener("click", function () {
+  listaSlide[currentIndex].classList.remove("visible");
+  currentIndex += 1;
+  if (currentIndex > 4) {currentIndex = 0}
+  listaSlide[currentIndex].classList.add("visible");
+});
+
+const prev = document.querySelector(".prev");
+prev.addEventListener("click", function () {
+    
+    listaSlide[currentIndex].classList.remove("visible");
+    currentIndex -= 1;
+    if (currentIndex < 0) {currentIndex = 4}
+    listaSlide[currentIndex].classList.add("visible");
+    
+    
+})
+
+
+
+
+
+
+
+
